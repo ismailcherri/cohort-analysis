4195412a4be86cac3da337c0be874830fe3d2eaf
Ismail Cherri <ismailcherri@gmail.com>
2019-07-07 16:45:55 +0200

    Optimizes front end js and css loading

--- resources/views/retention-stats.blade.php
+++ resources/views/retention-stats.blade.php
@@ -9,5 +9,7 @@
 @endsection
 
 @section('scripts')
+    <script src="{{ mix('js/manifest.js') }}"></script>
+    <script src="{{ mix('js/vendor.js') }}"></script>
     <script src="{{ mix('js/app.js') }}"></script>
 @endsection
